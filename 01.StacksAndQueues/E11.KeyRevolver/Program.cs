21107434c348e125a7bef455db8b28b49287f6f6
ezopus <133430790+ezopus@users.noreply.github.com>
2023-09-17 23:15:14 +0300

    last solutions to stacks and queues exercises added

---
+++ 01.StacksAndQueues/E11.KeyRevolver/Program.cs
@@ -0,0 +1,64 @@
+﻿int bulletPrice = int.Parse(Console.ReadLine());
+int barrelSize = int.Parse(Console.ReadLine());
+Stack<int> bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+Queue<int> locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+int reward = int.Parse(Console.ReadLine());
+
+int shotsFired = 0;
+
+while (bullets.Count > 0)
+{
+    int currentLock = locks.Peek();
+    int currentBullet = bullets.Pop();
+
+    if (currentBullet <= currentLock)
+    {
+        Console.WriteLine("Bang!");
+        locks.Dequeue();
+    }
+    else
+    {
+        Console.WriteLine("Ping!");
+    }
+    shotsFired++;
+    if (shotsFired % barrelSize == 0 && bullets.Any())
+    {
+        Console.WriteLine("Reloading!");
+    }
+
+    if (!locks.Any())
+    {
+        break;
+    }
+}
+
+if (bullets.Count >= 0 && !locks.Any())
+{
+    Console.WriteLine($"{bullets.Count} bullets left. Earned ${reward - shotsFired * bulletPrice}");
+}
+else
+{
+    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+}
+
+
+/*
+50
+2
+11 10 5 11 10 20
+15 13 16
+1500
+
+20
+6
+14 13 12 11 10 5
+13 3 11 10
+800
+
+33
+1
+12 11 10
+10 20 30
+100
+
+*/
---
+++ 01.StacksAndQueues/E12.CupsAndBottles/Program.cs
@@ -0,0 +1,47 @@
+﻿Queue<int> cups = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+Stack<int> bottles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+int wastedWater = 0;
+
+while (bottles.Any() && cups.Any())
+{
+    int currentCup = cups.Peek();
+    while (currentCup > 0)
+    {
+        if (currentCup <= bottles.Peek())
+        {
+            wastedWater += bottles.Pop() - currentCup;
+            cups.Dequeue();
+            currentCup = 0;
+        }
+        else
+        {
+            currentCup -= bottles.Pop();
+        }
+    }
+}
+
+if (bottles.Any() && !cups.Any())
+{
+    Console.WriteLine("Bottles: " + string.Join(" ", bottles));
+}
+else if (cups.Any() && !bottles.Any())
+{
+    Console.WriteLine("Cups: " + string.Join(" ", cups));
+}
+
+Console.WriteLine($"Wasted litters of water: {wastedWater}");
+
+
+
+/*
+4 2 10 5
+3 15 15 11 6
+
+1 5 28 1 4
+3 18 1 9 30 4 5
+
+10 20 30 40 50
+20 11
+
+*/
