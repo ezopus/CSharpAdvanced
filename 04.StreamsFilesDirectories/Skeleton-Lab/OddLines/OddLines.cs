90274f8270ac025a68d37fa59fe0e46c09d9f1bc
ezopus <133430790+ezopus@users.noreply.github.com>
2023-09-24 21:26:00 +0300

    streams, files, directories lab started

---
+++ 04.StreamsFilesDirectories/Skeleton-Lab/OddLines/OddLines.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int counter = 0;
+                    while (reader.EndOfStream == false)
+                    {
+                        string line = reader.ReadLine();
+                        if (counter++ % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
