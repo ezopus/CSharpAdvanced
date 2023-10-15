bd92e833b729138b7b0aaff34f1b8221ca6655a9
ezopus <133430790+ezopus@users.noreply.github.com>
2023-10-15 22:06:25 +0300

    streams and files exercises

---
+++ 04.StreamsFilesDirectories/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace CopyBinaryFile
+{
+    using System;
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream inputFile = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (FileStream outputFile = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024];
+
+                    int size = 0;
+
+                    while ((size = inputFile.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        outputFile.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+    }
+}
